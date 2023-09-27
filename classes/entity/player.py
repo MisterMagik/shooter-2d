eedf8302cc04b4403a48596f33ba7ff728d74fdc
lion1503 <lion1503@vp.pl>
2023-09-27 15:52:55 +0200

    delete folder classes

--- classes/entity/player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    id_s = 0
-    def __init__(self, name):
-        self.name = name
-        Player.id_s+=1
-        self.id = Player.id_s
- 
-    def __str__(self):
-        return f"{self.id}"
